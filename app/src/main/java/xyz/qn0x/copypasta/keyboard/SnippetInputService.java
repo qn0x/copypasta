e71f3460a76d67550823cdae109a5f9bbc146959
Flauschebauch <tanja.noack@yahoo.de>
2018-05-24 15:16:13 +0200

    Prototyp des Keyboards (keine Snipetfunktion bisher) hinzugefügt

--- app/src/main/java/xyz/qn0x/copypasta/keyboard/SnippetInputService.java
+++ app/src/main/java/xyz/qn0x/copypasta/keyboard/SnippetInputService.java
@@ -1,22 +1,94 @@
 package xyz.qn0x.copypasta.keyboard;
 
 import android.inputmethodservice.InputMethodService;
+import android.inputmethodservice.Keyboard;
+import android.inputmethodservice.KeyboardView;
+import android.text.TextUtils;
 import android.view.KeyEvent;
 import android.view.View;
+import android.view.inputmethod.InputConnection;
+
+import xyz.qn0x.copypasta.R;
 
 
-public class SnippetInputService extends InputMethodService {
+public class SnippetInputService extends InputMethodService implements KeyboardView.OnKeyboardActionListener {
     @Override
     public View onCreateInputView() {
 //        SnippetKeyboardView inputView = getLayoutInflater().inflate(R.layout.input, null);
 //        inputView.setOnKeyboardActionListener(this);
 //        inputView.setKeyboard();
 //        return inputView;
-        return null;
+//        return null;
+
+        KeyboardView keyboardView = (KeyboardView) getLayoutInflater().inflate(R.layout.keyboard_view, null);
+        Keyboard keyboard = new Keyboard(this, R.xml.number_pad);
+        keyboardView.setKeyboard(keyboard);
+        keyboardView.setOnKeyboardActionListener(this);
+        return keyboardView;
     }
 
     @Override
     public boolean onKeyDown(int keyCode, KeyEvent event) {
         return false;
     }
+
+    @Override
+    public void onPress(int i) {
+
+    }
+
+    @Override
+    public void onRelease(int i) {
+
+    }
+
+    @Override
+    public void onKey(int primatyCode, int[] keyCodes) {
+        InputConnection inputConnection = getCurrentInputConnection();
+
+        if (inputConnection != null) {
+            switch(primatyCode) {
+                case Keyboard.KEYCODE_DELETE :
+                    CharSequence selectedText = inputConnection.getSelectedText(0);
+
+                    if (TextUtils.isEmpty(selectedText)) {
+                        inputConnection.deleteSurroundingText(1, 0);
+                    } else {
+                        inputConnection.commitText("", 1);
+                    }
+
+                    break;
+                default :
+                    char code = (char) primatyCode;
+                    inputConnection.commitText(String.valueOf(code), 1);
+            }
+        }
+    }
+
+    @Override
+    public void onText(CharSequence charSequence) {
+
+    }
+
+    @Override
+    public void swipeLeft() {
+
+    }
+
+    @Override
+    public void swipeRight() {
+
+    }
+
+    @Override
+    public void swipeDown() {
+
+    }
+
+    @Override
+    public void swipeUp() {
+
+    }
 }
+
+
