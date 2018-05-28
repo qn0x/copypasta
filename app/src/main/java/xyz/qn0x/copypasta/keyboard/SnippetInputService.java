0143f7a55acf4d507cdc96cf877334c226f72582
Janine Kostka <janine@qn0x.xyz>
2018-05-28 09:45:07 +0200

    keyboard fügt text ein

--- app/src/main/java/xyz/qn0x/copypasta/keyboard/SnippetInputService.java
+++ app/src/main/java/xyz/qn0x/copypasta/keyboard/SnippetInputService.java
@@ -4,6 +4,7 @@ import android.inputmethodservice.InputMethodService;
 import android.inputmethodservice.Keyboard;
 import android.inputmethodservice.KeyboardView;
 import android.text.TextUtils;
+import android.util.Log;
 import android.view.KeyEvent;
 import android.view.View;
 import android.view.inputmethod.InputConnection;
@@ -12,20 +13,30 @@ import xyz.qn0x.copypasta.R;
 
 
 public class SnippetInputService extends InputMethodService implements KeyboardView.OnKeyboardActionListener {
+
+    private final static String TAG = "SnippetInputService";
+
     @Override
     public View onCreateInputView() {
-//        SnippetKeyboardView inputView = getLayoutInflater().inflate(R.layout.input, null);
-//        inputView.setOnKeyboardActionListener(this);
-//        inputView.setKeyboard();
-//        return inputView;
-//        return null;
 
         KeyboardView keyboardView = (KeyboardView) getLayoutInflater().inflate(R.layout.keyboard_view, null);
         Keyboard keyboard = new Keyboard(this, R.xml.number_pad);
+
+
+        // TODO hier Snippets aus der Datenbank ziehen und bei Key als android:keyOutputText setzen
+        // TODO den Namen als keyLabel nehmen
+
+        keyboard.getKeys().forEach(key -> {
+            key.text = "Hello World!";
+        });
+
+
         keyboardView.setKeyboard(keyboard);
         keyboardView.setOnKeyboardActionListener(this);
+
         return keyboardView;
     }
+
 
     @Override
     public boolean onKeyDown(int keyCode, KeyEvent event) {
@@ -43,12 +54,14 @@ public class SnippetInputService extends InputMethodService implements KeyboardV
     }
 
     @Override
-    public void onKey(int primatyCode, int[] keyCodes) {
+    public void onKey(int primaryCode, int[] keyCodes) {
+
+
         InputConnection inputConnection = getCurrentInputConnection();
 
         if (inputConnection != null) {
-            switch(primatyCode) {
-                case Keyboard.KEYCODE_DELETE :
+            switch (primaryCode) {
+                case Keyboard.KEYCODE_DELETE:
                     CharSequence selectedText = inputConnection.getSelectedText(0);
 
                     if (TextUtils.isEmpty(selectedText)) {
@@ -58,21 +71,26 @@ public class SnippetInputService extends InputMethodService implements KeyboardV
                     }
 
                     break;
-                default :
-                    char code = (char) primatyCode;
+                default:
+                    char code = (char) primaryCode;
                     inputConnection.commitText(String.valueOf(code), 1);
             }
+
         }
     }
 
     @Override
     public void onText(CharSequence charSequence) {
-
+        Log.d(TAG, "KeyOutputText: " + charSequence.toString());
+        InputConnection inputConnection = getCurrentInputConnection();
+        if (inputConnection != null) {
+            inputConnection.commitText(charSequence.toString(), 1);
+        }
     }
 
     @Override
     public void swipeLeft() {
-
+        //TODO evtl Delete auf den eingefügten Text machen
     }
 
     @Override
